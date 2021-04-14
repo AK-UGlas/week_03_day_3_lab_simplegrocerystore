4f388b3b0b39ec6a13c922eaca9f048ace012a73
Allen_K <allenkelly278@gmail.com>
2021-04-14 15:48:14 +0100

    added links and second page

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,4 +4,9 @@ from models.online_groceries import orders
 
 @app.route('/orders')
 def index():
-    return render_template('index.html', title='Home', orders=orders)
+    return render_template('index.html', title='Home', orders=orders, order_len=len(orders))
+
+@app.route('/orders/<index>')
+def display_order(index):
+    order = orders[int(index)]
+    return render_template('order.html', title='Specific order', order_to_display=order)
--- models/online_groceries.py
+++ models/online_groceries.py
@@ -2,5 +2,7 @@ from models.order import Order
 
 customer1 = Order("Allen", "14-Apr-2021", 1, "milk")
 customer2 = Order("Kieran", "13-Apr-2021", 2, "bread")
+customer3 = Order("Dan", "10-Mar-2021", 5, "toilet paper")
+customer4 = Order("Hannah", "10-Apr-2021", 3, "bread, jam, milk")
 
-orders = [customer1, customer2]
+orders = [customer1, customer2, customer3, customer4]
